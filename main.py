c2aaf1d3a7f703425bcff530a625c8eaff0baa06
madewithdan <greatdan0419@gmail.com>
2024-03-01 06:47:32 -0800

    new commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Airtime purchase")
+print(" a.mtn\n b.airtel\n c.9mobile\n d.Glo\n")
+response = str(input(" ")).lower()
+mobile_number = input("please enter your phone number:")
+airtime_cost = int(input("please enter the amount of airtime you want to purchase:"))
+print("you have succesfully purchase your #{airtime_cost} for airtime of {mobile_number}:")
+# data purchase
+print("Data purchase")
+print(" a.mtn\n b.airtel\n c.9mobile\n d.Glo\n")
+Data = input("please input mobile number:")
+Data_cost = int(input("please enter the amount of data you want to purchase {amount of Data}"))
+print("you have successfully purchase {data_cost} for {mobile number}:")
